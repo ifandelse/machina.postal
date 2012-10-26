0d6922d7c350e61d1c5e26bcbf7d4b6930e76e4d
Jim Cowart <ashbylane@gmail.com>
2012-10-26 00:57:18 -0400

    Converted tests to mocha+expect

--- spec/postalintegration.spec.js
+++ spec/postalintegration.spec.js
@@ -9,69 +9,135 @@ var testCapture = {
 	"fsm.events.OnEnter" : false
 };
 
-QUnit.specify( "machina.js integration with postal.js", function () {
-	var fsm;
-	describe( "With custom FSM", function () {
-		fsm = new machina.Fsm( {
-			initialState : "uninitialized",
-			namespace : "myFsm",
-			states : {
-				"uninitialized" : {
-					"event1" : function () {
-						this.transition( "initialized" );
-					},
-					"event2" : function () {
-						this.deferUntilTransition();
-					}
+var fsm;
+describe( "With custom FSM", function () {
+	fsm = new machina.Fsm( {
+		initialState : "uninitialized",
+		namespace : "myFsm",
+		states : {
+			"uninitialized" : {
+				"event1" : function () {
+					this.transition( "initialized" );
 				},
-				"initialized" : {
-					_onEnter : function () {
-						this.fireEvent( "OnEnter" );
-					},
-					"event2" : function () {
-						this.fireEvent( "CustomEvent" );
-					},
-					"event3" : function () {
+				"event2" : function () {
+					this.deferUntilTransition();
+				}
+			},
+			"initialized" : {
+				_onEnter : function () {
+					this.fireEvent( "OnEnter" );
+				},
+				"event2" : function () {
+					this.fireEvent( "CustomEvent" );
+				},
+				"event3" : function () {
 
-					}
 				}
 			}
-		} );
-		postal.subscribe( { channel : "myFsm", topic : "#", callback : function ( data, envelope ) {
-			testCapture[envelope.topic] = true;
-		}} );
-		postal.subscribe( { channel : "myFsm.events", topic : "#", callback : function ( data, envelope ) {
-			testCapture[envelope.topic] = true;
-		}} );
+		}
+	} );
+	postal.subscribe( { channel : "myFsm", topic : "#", callback : function ( data, envelope ) {
+		testCapture[envelope.topic] = true;
+	}} );
+	postal.subscribe( { channel : "myFsm.events", topic : "#", callback : function ( data, envelope ) {
+		testCapture[envelope.topic] = true;
+	}} );
 
-		postal.publish( { channel : "myFsm", topic : "event21"} );
-		postal.publish( { channel : "myFsm", topic : "event2" } );
-		postal.publish( { channel : "myFsm", topic : "event1" } );
-		fsm.transition( "NoSuchThing" );
+	postal.publish( { channel : "myFsm", topic : "event21"} );
+	postal.publish( { channel : "myFsm", topic : "event2" } );
+	postal.publish( { channel : "myFsm", topic : "event1" } );
+	fsm.transition( "NoSuchThing" );
 
-		it( "should fire the transition event", function () {
-			assert( testCapture["transition"] ).equals( true );
-		} );
-		it( "should fire the nohandler event", function () {
-			assert( testCapture["nohandler"] ).equals( true );
-		} );
-		it( "should fire the handling event", function () {
-			assert( testCapture["handling"] ).equals( true );
-		} );
-		it( "should fire the handled event", function () {
-			assert( testCapture["handled"] ).equals( true );
-		} );
-		it( "should fire the CustomEvent event", function () {
-			assert( testCapture["CustomEvent"] ).equals( true );
-		} );
-		it( "should fire the OnEnter handler", function () {
-			assert( testCapture["OnEnter"] ).equals( true );
-		} );
-		it( "should fire the invalidstate handler", function () {
-			assert( testCapture["invalidstate"] ).equals( true );
-		} );
-		it( "should fire the deferred handler", function () {
-			assert( testCapture["deferred"] ).equals( true );
-		} );
+	it( "should fire the transition event", function () {
+		expect( testCapture["transition"] ).to.be( true );
+	} );
+	it( "should fire the nohandler event", function () {
+		expect( testCapture["nohandler"] ).to.be( true );
+	} );
+	it( "should fire the handling event", function () {
+		expect( testCapture["handling"] ).to.be( true );
+	} );
+	it( "should fire the handled event", function () {
+		expect( testCapture["handled"] ).to.be( true );
+	} );
+	it( "should fire the CustomEvent event", function () {
+		expect( testCapture["CustomEvent"] ).to.be( true );
+	} );
+	it( "should fire the OnEnter handler", function () {
+		expect( testCapture["OnEnter"] ).to.be( true );
+	} );
+	it( "should fire the invalidstate handler", function () {
+		expect( testCapture["invalidstate"] ).to.be( true );
+	} );
+	it( "should fire the deferred handler", function () {
+		expect( testCapture["deferred"] ).to.be( true );
+	} );
+} );
+
+describe( "With Multiple FSMs", function () {
+	fsm = new machina.Fsm( {
+		initialState : "uninitialized",
+		namespace : "myFsmA",
+		states : {
+			"uninitialized" : {
+				"event1" : function () {
+					this.transition( "initialized" );
+				},
+				"event2" : function () {
+					this.deferUntilTransition();
+				}
+			},
+			"initialized" : {
+				_onEnter : function () {
+					this.fireEvent( "OnEnter" );
+				},
+				"event2" : function () {
+					this.fireEvent( "CustomEvent" );
+				},
+				"event3" : function () {
+
+				}
+			}
+		}
+	} );
+
+	var fsm2Events = {};
+
+	var fsm2 = new machina.Fsm( {
+		initialState : "uninitialized",
+		namespace : "myFsm2",
+		states : {
+			"uninitialized" : {
+				"event1" : function () {
+					fsm2Events.event1 = true;
+					this.transition( "initialized" );
+				},
+				"event2" : function () {
+					this.deferUntilTransition();
+				}
+			},
+			"initialized" : {
+				_onEnter : function () {
+					this.fireEvent( "OnEnter" );
+				},
+				"event2" : function () {
+					fsm2Events.event2 = true;
+					this.fireEvent( "CustomEvent" );
+				},
+				"event3" : function () {
+					fsm2Events.event3 = true;
+				}
+			}
+		}
+	} );
+
+	postal.publish( { channel : "myFsmA", topic : "event2" } );
+	postal.publish( { channel : "myFsmA", topic : "event1" } );
+	fsm.transition( "NoSuchThing" );
+
+	it( "should NOT fire events in the second FSM the deferred handler", function () {
+		expect( !!fsm2Events["event1"] ).to.be( false );
+		expect( !!fsm2Events["event2"] ).to.be( false );
+		expect( !!fsm2Events["event3"] ).to.be( false );
 	} );
 } );
